0689e86b16d32bfcf926c421645c683e4faafe75
KaranDya <karandahiya456@gmail.com>
2024-10-17 23:48:14 -0700

    lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,7 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Psuedocode for Connect 4-lab 4 
+// Author: Karan Dahiya
+// Date: Oct 17 2024
+
 
 // Constants
 
@@ -19,3 +20,28 @@ function main() {
 
 // let's get this party started
 main();
+//Psuedocode:
+    //Print the board before each turn
+      //Loop through each row
+      //Loop through each space
+      //Print X, O, or space
+      //Print a new line at the end
+      //Print a horizontal line in between
+    //Accept user input
+      //Ask user for input
+      //Proceed
+    //Validate user move
+      //Make error message if move is invalid
+      //Proceed normally if not
+      //Update the board (Add user’s icon: O)
+    //Evaluate possible moves
+      //Check columns if they have an empty space
+      //Mark columns with empty space valid
+    //Choose computer move
+      //Choose valid move
+      //Update the board (Add computer’s icon: X)
+    //Test for win or draw condition
+      //Win Condition: 4 icons are in a row horizontally, vertically, or diagonally
+      //Draw Condition: No more possible moves, and win condition isn’t reached.
+
+
