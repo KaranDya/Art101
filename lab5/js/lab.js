8baf28bdb6ef22df499957c0c0f7de0fb672a293
KaranDya <karandahiya456@gmail.com>
2024-10-22 23:47:30 -0700

    lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,32 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5: Data Types and Variables
+// Author: Karan Dahiya
+// Date: 22 Oct 2024
+//Domain: Public
 
-// Constants
 
-// Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
 
-// let's get this party started
-main();
+
+
+//Variables
+make = "Honda";
+model = "Accord";
+color = "Black";
+year = "2022";
+
+
+
+
+//calculations
+age = 2024 - year;
+
+
+
+
+//Outputs
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
