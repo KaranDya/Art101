c168495d9bed6129e1ad620ab9f392ef5238488a
KaranDya <karandahiya456@gmail.com>
2024-10-28 22:49:50 -0700

    java lab 7

---
+++ lab7/js/lab7.js
@@ -0,0 +1,28 @@
+// index.js - Functions! 
+// Author: Karan Dahiya
+// Date: Oct 28, 2024
+
+//variables
+let 
+
+//function
+function usernameinput () {
+    
+    let userName = prompt("My name is");
+    console.log("userName =", username);
+    //splittitng the string
+    var nameArray = userName.split('');
+    console.log("nameArray =", nameArray);
+    //sorting the array
+    var nameArraySort = nameArray.sort();
+    console.log("nameArraySort =", nameArraySort);
+    //joing the array back into a string. 
+    var nameSorted = nameArraySort.join('');
+    console.log("nameSorted =", nameSorted);
+    
+    //get the sorted name.
+    return nameSorted;
+}
+//the output
+document.writeln("Your name: ", usernameinput(), "<br>");
+
