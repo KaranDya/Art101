dbe7a40c832b4f2b85d9f4d444d4c10d79fbe0a6
KaranDya <karandahiya456@gmail.com>
2024-10-28 23:14:11 -0700

    Update lab7.js

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -8,14 +8,26 @@
 function usernameinput () {
     
     let userName = prompt("My name is");
-    console.log("userName =", username);
+    console.log("userName =", userName);
+   
+   
     //splittitng the string
+   
+   
     var nameArray = userName.split('');
     console.log("nameArray =", nameArray);
+    
+    
     //sorting the array
+    
+    
     var nameArraySort = nameArray.sort();
     console.log("nameArraySort =", nameArraySort);
+    
+    
     //joing the array back into a string. 
+   
+   
     var nameSorted = nameArraySort.join('');
     console.log("nameSorted =", nameSorted);
     
