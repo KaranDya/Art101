0e2dc42c4433c93e9ad0df3f0473dddbb3a5e078
KaranDya <karandahiya456@gmail.com>
2024-11-04 23:27:56 -0800

    lab 9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -8,30 +8,13 @@
 */
 
 function  main(){
-    //create a button
-    // add button to challenge section
-    $("#challenge").append("<button id='button-challenge'>Make Special</button>");
-    // add button to problems section
-    $("#problems").append("<button id='button-problems'>Make Special</button>");
-    // add button to reflection section
-    $("#reflection").append("<button id='button-reflection'>Make Special</button>");
-
-    // add a click listener to the challenge button
-    $("#button-challenge").click(function(){
+    // new code
+  // add button to all three sections
+  $(".special_section").append("<button class='special-sections-button'>Make Special</button>");
+  // add a click listener to the button
+  $(".special-sections-button").click(function(){
     // now add (or subtract) the "special" class to the section
-    $("#challenge").toggleClass("special");
-    });
-    // add a click listener to the challenge button
-    $("#button-problems").click(function(){
-    // now add (or subtract) the "special" class to the section
-    $("#problems").toggleClass("special");
-    });
-    // add a click listener to the challenge button
-    $("#button-reflection").click(function(){
-    // now add (or subtract) the "special" class to the section
-    $("#reflection").toggleClass("special");
-    }); 
-    $("#special_section").append("<button id='special-button'>Make Special</button>");
-    $("#special-button").click
-}
+    $(this).parent().toggleClass("special");
+  });
+ };
 main()
