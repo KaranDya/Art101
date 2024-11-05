e22c2ab45c5ae5c92570da38a9004207becdd86c
KaranDya <karandahiya456@gmail.com>
2024-11-04 22:16:21 -0800

    lab 9

--- lab 9/js/lab.js
+++ lab 9/js/lab.js
@@ -10,7 +10,7 @@
 $("#challenge").append("<button id='button-challenge'>Make Special</button>");
 
 // add a click listener to the challenge button
-$("button-challenge").click(function(){
+$("#button-challenge").click(function(){
     // now add (or subtract) the "special" class to the section
     $("#challenge").toggleClass("special");
 });
@@ -18,15 +18,15 @@ $("button-challenge").click(function(){
 $("#problems").append("<button id='button-problems'>Make Special</button>");
 
 // add a click listener to the challenge button
-$("button-problems").click(function(){
+$("#button-problems").click(function(){
     // now add (or subtract) the "special" class to the section
     $("#problems").toggleClass("special");
 });
 
-$("#reflection").append("<button id='button-challenge'>Make Special</button>");
+$("#reflection").append("<button id='button-reflection'>Make Special</button>");
 
 // add a click listener to the challenge button
-$("button-relection").click(function(){
+$("#button-reflection").click(function(){
     // now add (or subtract) the "special" class to the section
     $("#reflection").toggleClass("special");
 });
