97dd2432a8e3d8d5bb94cac487c0f347a2392bdb
KaranDya <karandahiya456@gmail.com>
2024-11-04 21:48:59 -0800

    lab 9

---
+++ lab 9/js/lab.js
@@ -0,0 +1,32 @@
+/*
+   lab.js - This simple JavaScript/jQuery script uses buttons to modify some elements on the page
+
+   Requirements: jQuery must be loaded for this script to work.
+
+   Author: Wes modes
+   Date: 2023
+*/
+
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+
+// add a click listener to the challenge button
+$("button-challenge").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#challenge").toggleClass("special");
+});
+
+$("#problems").append("<button id='button-problems'>Make Special</button>");
+
+// add a click listener to the challenge button
+$("button-problems").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#problems").toggleClass("special");
+});
+
+$("#reflection").append("<button id='button-challenge'>Make Special</button>");
+
+// add a click listener to the challenge button
+$("button-relection").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#reflection").toggleClass("special");
+});
